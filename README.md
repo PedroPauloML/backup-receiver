4e06606e9cc8f44b9aef86a68f8da327b6df624e
Pedro Paulo Marques de Lima <pedropaulomarqz@gmail.com>
2019-09-23 12:00:32 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Backup Receiver
+
+This application is a simple way to send especifics data from server to localhost, whithout .dump or
+similars backups. This application work with data format JSON.
+
+## Prerequisites
+
+Sinatra: `gem install sinatra`
+Ngrok: (https://ngrok.com/download)[https://ngrok.com/download].
+
+## Getting started
+
+First, run the sinatra server: `path/to/application ruby app.rb`.
+
+Then, run ngrok server: `path/to/application ./ngrok http 3000`
+
+Send data to route of ngrok with the URI `/backups`, like this:
+
+```ruby
+# Ruby Language
+
+require 'net/http'
+require 'net/https' # for ruby 1.8.7
+require 'json'
+
+data = JSON.parse({
+  data: {
+    filename: "Example",
+    key1: "value1",
+    key2: "value2",
+  }.to_json
+}.to_json)
+
+ngrok_route = "[ngrok route]"
+url = URI.parse(ngrok_route + "/backups")
+req = Net::HTTP::Post.new(url.request_uri)
+req.set_form_data(data)
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = (url.scheme == "https")
+response = http.request(req)
+if response.code == "200"
+  body = JSON.parse(response.body)
+  `open #{ngrok_route + body["file_uri"]}`
+else
+  puts response.body
+end
+```
---
+++ app.rb
@@ -0,0 +1,78 @@
+require 'sinatra'
+require 'json'
+require 'yaml/store'
+require 'i18n'
+require 'rubygems'
+require 'active_support/all'
+
+# Routes
+get '/' do
+  return "Backup Receiver"
+end
+
+post '/backups' do
+  # PARAMETERS STRUCTURE
+  # 
+  # {
+  #   "data": {
+  #     "filename": "...",
+  #     "key1": "value1",
+  #     "key2": "value2",
+  #   }
+  # }
+
+  content_type :json
+
+  puts params.inspect
+  if params["data"].present?
+    data = JSON.parse(params["data"])
+    if data["filename"].present?
+      directory_name = File.join(File.dirname(__FILE__), "backups")
+      Dir.mkdir(directory_name) unless File.exists?(directory_name)
+      
+      data_file_name = data["filename"].parameterize
+      file_name = "#{Time.now.strftime("%y%m%d%H%M%S")}-#{data_file_name}.json"
+      file_path = File.join(
+        File.dirname(__FILE__),
+        "backups",
+        file_name
+      )
+      
+      data.delete("filename")
+
+      data = { data_file_name.to_s => data }
+
+      begin
+        File.open(file_path, 'wb') do |f|
+          f.write(data.to_json)
+        end
+
+        return { result: "Successfully", file_uri: "/backups/#{file_name}" }.to_json
+      rescue Exception => ex
+        halt(500, ex)
+      end
+    else
+      halt(
+        400,
+        "The JSON should has a parameter key called \"filename\" with a name to use as file name."
+      )
+    end
+  else
+    halt(
+      400,
+      "The data should be send into a parameter key called \"data\"."
+    )
+  end
+end
+
+get '/backups/:id' do
+  content_type :json
+
+  directory_name = File.join(File.dirname(__FILE__), "backups", params[:id])
+  if File.exists?(directory_name)
+    f = File.read(directory_name)
+    return f
+  else
+    halt(404, "File not found")
+  end
+end
